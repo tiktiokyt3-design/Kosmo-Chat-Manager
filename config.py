4a6b1d8944b92e6c1eafbb4d765dd8b8fa7c640a
tapuh0code <tiktiokyt3@gmail.com>
2026-04-01 03:28:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 # Kosmo_cm_bot 1v.0
 # =============
 
-token = '8411158818:AAHfsGwCz80NcrmTcCVVi0m-bChX1ex5xek'
-owner_id = 6994614587 # @tapuh0
+token = 'ВАШ ТОКЕН'
+owner_id = "ВАШ АЙДИ ТОЛЬКО БЕЗ ЛАПОК" # @tapuh0
 manager_id = None
-brather_id = None # 5260422001 - @Dodep_4444
-secret_id = 5261677517 # secret
+brather_id = None #
+secret_id = None # secret
