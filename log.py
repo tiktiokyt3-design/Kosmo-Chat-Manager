180d1bf44a8c30f2d1c53ec5e7668b7805c606e1
tapuh0code <tiktiokyt3@gmail.com>
2026-04-01 03:39:17 +0300

    Delete log.py

--- log.py
+++
@@ -1,9 +0,0 @@
-import time
-from datetime import datetime
-
-log_file = 'logger.log'
-
-def log(action):
-    with open(log_file, 'a', encoding="utf-8") as f:
-        f.write(f"[{datetime.now()}]: {action}\n\n")
-        f.close()    
